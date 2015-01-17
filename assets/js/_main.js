0d8194910fd4fb880ff6c7c90f516c837d90ada0
iboss <supanat.ptk@gmail.com>
2015-01-17 10:26:30 +0700

    no msg

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -39,8 +39,40 @@ var Roots = {
     init: function() {
       // JavaScript to be fired on the home page
       // height equal viewport
+
+      // this section is only for test purpose
+      // ===== BEGIN =====
+      //! jQuery Instagram - v0.3.1 - 2014-06-19
+      !function(a){function b(b){var c="https://api.instagram.com/v1",d={};if(null==b.accessToken&&null==b.clientId)throw"You must provide an access token or a client id";if(d=a.extend(d,{access_token:b.accessToken||"",client_id:b.clientId||"",count:b.count||""}),null!=b.url)c=b.url;else if(null!=b.hash)c+="/tags/"+b.hash+"/media/recent";else if(null!=b.search)c+="/media/search",d=a.extend(d,b.search);else if(null!=b.userId){if(null==b.accessToken)throw"You must provide an access token";c+="/users/"+b.userId+"/media/recent"}else null!=b.location?(c+="/locations/"+b.location.id+"/media/recent",delete b.location.id,d=a.extend(d,b.location)):c+="/media/popular";return{url:c,data:d}}a.fn.instagram=function(c){var d=this;c=a.extend({},a.fn.instagram.defaults,c);var e=b(c);return a.ajax({dataType:"jsonp",url:e.url,data:e.data,success:function(a){d.trigger("didLoadInstagram",a)}}),this.trigger("willLoadInstagram",c),this},a.fn.instagram.defaults={accessToken:null,clientId:null,count:null,url:null,hash:null,userId:null,location:null,search:null}}(jQuery);
+      // ====== END ======
+
+
+      $('.instagram').on('willLoadInstagram', function(event, options) {
+        // console.log(options);
+      });
+      $('.instagram').on('didLoadInstagram', function(event, response) {
+        var igfeed = response.data;
+        var igimgcount = 6;
+        for (var i = igfeed.length - 1; i >= 0; i--) {
+          if ( igimgcount <= 0 ) break;
+          igimgcount--;
+            if ( igfeed[i].images !== undefined ) { 
+              var imgurl = igfeed[i].images.standard_resolution.url;
+              // console.log("image");
+              // console.log(igfeed[i]);
+              $('.instagram').append("<a href='" + igfeed[i].link + "' target='_blank'><img class='small-6 no-pad' id='"+ igfeed[i].id +"' src = '" + imgurl + "'></img></a>")
+            }
+            else if ( igfeed[i].video !== undefined ) console.log("video");
+        };
+      });
+      $('.instagram').instagram({
+        // hashtag
+        hash: 'cityscape',
+        clientId: '77995f907c4348909e35165138fd2e62'
+      });
+      
       function meet_viewport_height(element) {
-        var vp_height = $(window).height()*1.15;
+        var vp_height = $(window).height();
         $(element).css({ height: vp_height });
       }
 
@@ -80,7 +112,8 @@ var Roots = {
         var transparency = 0.5+(scroll/(3*header_height));
         $overlay.css({ opacity: transparency });
       });
-    }
+    },
+
   },
 
   // About us page, note the change from about-us to about_us.
