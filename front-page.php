2cb6d852f2c87beb13550271b08bc4c75eca05ea
Supanat Potiwarakorn <supanat.ptk@gmail.com>
2015-03-15 20:17:21 +0700

    add event calendar

--- front-page.php
+++ front-page.php
@@ -15,7 +15,7 @@
 	</div>
 </header>
 
-<!--
+
  <section id="news">
 
 	<div class="small-12 no-pad section-title section-title-news rows">
@@ -83,18 +83,3 @@
 </section>
 </div> 
 
--->
-
-<!-- <section id="content">
-<h1>&nbsp</h1>
-	<h1 id="one">One</h1>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus non urna a malesuada. In ac lorem vel nisi imperdiet cursus. Curabitur facilisis rutrum tortor, id rutrum lacus facilisis et. Phasellus non ipsum lobortis, tempor elit a, hendrerit nulla. Aenean semper facilisis augue id porttitor. Sed accumsan nibh a diam molestie, eu egestas mauris venenatis. Duis scelerisque luctus enim et gravida. Nam quis turpis non purus interdum luctus. Etiam gravida adipiscing venenatis.</p>
-	<p>Duis non est massa. Integer placerat dictum magna et dapibus. Proin laoreet, neque eu tempus aliquam, eros orci consequat ante, in consectetur ligula magna vel justo. Nulla eu quam egestas, vehicula mi a, auctor dui. Maecenas nec sem sit amet velit interdum ullamcorper at non diam. Vestibulum lectus orci, tempor eu scelerisque vitae, vulputate id tortor. Cras lacus dui, aliquet at tempus pharetra, ultrices ac eros. Integer a placerat quam, sed ornare tellus. In eu placerat massa. Phasellus accumsan faucibus leo, sed egestas eros accumsan ac. Praesent adipiscing, nulla et ullamcorper suscipit, lectus magna rhoncus augue, at sodales eros lectus eu risus. Nam vulputate sem ac faucibus venenatis. Aenean semper felis sed convallis cursus. Proin commodo magna gravida dolor egestas, vitae congue dolor vestibulum. Nulla eros purus, consectetur eu sollicitudin eget, porttitor eu tortor.</p>
-
-	<h1 id="two">Two</h1>
-	<p>Proin convallis tortor ac fringilla volutpat. Quisque imperdiet a urna sit amet venenatis. Vestibulum tempus aliquam porta. Cras molestie condimentum leo, vitae consequat risus adipiscing porta. Mauris laoreet lacus vel pharetra tristique. Sed mollis ut urna vel vulputate. Integer sodales sollicitudin vestibulum. In mauris ante, tristique sit amet euismod sit amet, fermentum eu ligula. Curabitur sagittis lorem in lobortis elementum. Nunc ac velit sed quam scelerisque aliquam sit amet vitae diam. Aliquam adipiscing tortor urna, a cursus est fringilla at. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-	<p>Nulla luctus scelerisque est, in cursus tellus consectetur quis. Donec a nibh nec tortor iaculis adipiscing ut id velit. Maecenas sodales elementum lectus, vitae consectetur massa fermentum at. Phasellus eu vulputate turpis, vel suscipit mauris. Integer rhoncus ullamcorper nisi. Mauris pretium purus lacus, vel molestie felis semper in. Mauris bibendum sem non ipsum condimentum, eget consequat odio venenatis. Vestibulum quis imperdiet neque, eu viverra diam. Nullam nulla massa, mattis in dui vitae, fermentum fringilla ipsum. Aenean accumsan hendrerit lorem, sed fermentum ipsum tristique a. Praesent mollis elit sed dignissim sodales. Nam ornare sed enim aliquet laoreet. Suspendisse potenti. Nulla metus mi, imperdiet eu nibh eget, tempor sollicitudin felis.</p>
-
-	<h1 id="three">Three</h1>
-	<p>In dapibus porta volutpat. Donec lobortis arcu et commodo sollicitudin. Sed non condimentum magna. Cras sit amet adipiscing magna. Sed volutpat nisl sed lectus dignissim vehicula. Proin molestie et quam eu porttitor. Etiam interdum, nisl sed venenatis consequat, turpis augue dignissim purus, eget rutrum est nisi sit amet velit. Praesent interdum congue neque, ac molestie mauris auctor rutrum. Nunc nibh ante, ultricies ut ornare quis, faucibus eget nunc.</p>
-</section> -->
---
+++ tribe-event/default-template.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Default Events Template
+ * This file is the basic wrapper template for all the views if 'Default Events Template'
+ * is selected in Events -> Settings -> Template -> Events Template.
+ *
+ * Override this template in your own theme by creating a file at [your-theme]/tribe-events/default-template.php
+ *
+ * @package TribeEventsCalendar
+ *
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+ ?>
+<section class="page-content bg-handmouse">
+	<div class="small-12 large-offset-1 large-10 paper-like-content-wrapper">
+	  	<div class="cover-title">
+	    	<h1>Event</h1>
+	    </div>
+	    
+	    <div id="tribe-events-pg-template" style="padding-top:0em;padding-bottom:2em;">
+		<?php tribe_events_before_html(); ?>
+		<?php tribe_get_view(); ?>
+		<?php tribe_events_after_html(); ?>
+		</div> <!-- #tribe-events-pg-template -->
+	 
+  	
+  	</div>
+</div>
+	
